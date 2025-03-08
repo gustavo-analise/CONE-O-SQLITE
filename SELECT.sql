f48024b59c05c46dc15a98d9300191a4f4dd91d5
Gustavo <gustavo.andrade2205@hotmail.com>
2025-03-08 08:29:50 -0300

    Update SELECT.sql

--- SELECT.sql
+++ SELECT.sql
@@ -1,8 +1,16 @@
--- SQLite
+-- Seleciona todos os registros da tabela CLIENTES
 SELECT * FROM CLIENTES;
+
+-- Seleciona todos os registros da tabela VENDAS
 SELECT * FROM VENDAS;
-SELECT C.ID,C.NOME,C.CPF,V.VALOR_VENDA,V.cliente_id
+
+-- Realiza um INNER JOIN entre as tabelas CLIENTES e VENDAS para trazer informações combinadas
+SELECT 
+    C.ID,         -- ID do cliente
+    C.NOME,       -- Nome do cliente
+    C.CPF,        -- CPF do cliente
+    V.VALOR_VENDA,-- Valor da venda realizada
+    V.cliente_id  -- ID do cliente na tabela VENDAS (chave estrangeira)
 FROM CLIENTES C
 INNER JOIN VENDAS V 
-ON C.Id = V.cliente_id; 
-
+ON C.Id = V.cliente_id; -- A condição de junção relacionando os clientes às suas respectivas vendas
